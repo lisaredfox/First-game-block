1a754523670f2444b42c65ba604e97a31d41c4c6
lisaredfox <79074708+lisaredfox@users.noreply.github.com>
2022-06-28 01:25:12 +0500

    num, score, question

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 let hello = alert('Привет!');
 
+let num = 5;
+let score = 'something';
+
 const questionDifficulty = {
     question: 'Какую сложность игры ты выберешь?',
     answer: 'легкая',
     correctAnswer: 'средняя'
-};
+}
 
 const questionGame = {
     question: 'ты любишь видеоигры?',
     answer: 'нет',
     correctAnswer: 'да'
-};
+}
 
 const questionAge = {
     question: 'How old are you?',
     answer: '7',
     correctAnswer: '18'
-};
+}
 
 const questionName = {
     question: 'What is your name?',
     answer: 'Elizaveta',
     correctAnswer:'Elizaveta'
-};
+}
